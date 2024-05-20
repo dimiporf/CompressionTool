e3a43d650d98e54529a88e12b6f0184dcf2915de
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-05-20 19:13:07 +0200

    Add project files.

---
+++ CompressionTool/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CompressionTool
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Path to the downloaded text file
+            string filePath = "/135-0.txt"; // Make sure this path is correct relative to your project directory
+            string text = File.ReadAllText(filePath);
+
+            // Calculate frequencies
+            var frequencies = CalculateFrequencies(text);
+
+            // Print frequencies (for testing)
+            foreach (var kvp in frequencies)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+        }
+
+        static Dictionary<char, int> CalculateFrequencies(string text)
+        {
+            var frequencyDict = new Dictionary<char, int>();
+            foreach (char c in text)
+            {
+                if (frequencyDict.ContainsKey(c))
+                {
+                    frequencyDict[c]++;
+                }
+                else
+                {
+                    frequencyDict[c] = 1;
+                }
+            }
+            return frequencyDict;
+        }
+    }
+}
